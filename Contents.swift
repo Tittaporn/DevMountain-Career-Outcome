b41a019a9232387c2e595ebd3ac62c3ec36db863
tittaporn <tittaporn_saelee@icloud.com>
2021-01-19 20:23:04 -0600

    try to pust to origin

---
+++ Contents.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+
+//Week 2 At DevMountain : Friday 15 Jan 2021
+
+/*
+ The Power of the Resume
+
+ Participation
+ Respect
+ Practice
+ Seek opportunities
+ 
+ Goal : Get Visible, Get the phone called.
+ 
+ _____________________________________________________________________________
+ 
+ - I am a professional and should be spoken to/ give the.. I am the real deal.
+ - The language you use in the resume is really important.
+ - Keywords are important. Using software to get the keywords.
+
+ _____________________________________________________________________________
+ 
+ The Audience for the Resume
+ - Recruiter. // Resume Needed to catch thier eyes.
+ - Hiring Manager. //People that work with us.
+ - Answer On the Resume : Why I come on this field?
+ - Do not ditch where you came from the resume. Your backgroung tells the same story with new story that they want to hear.
+ - The resume need to give a brand of you.
+ - 7 Seconds for Recruiters and for us to make the resume readable in 7 sec.
+ - Avoid Fuctional Resume and Reverse...???
+ _____________________________________________________________________________
+
+ Chronological >> Easy to read, parse well, servers the checklist function/purpose.
+ 
+ 1) Context Info
+ 2) Summary Statement/ Object Statement
+ 3) Skills
+ 4) Eduction
+ 5) Experience
+ 6) Volunteer Work / Hobbies
+
+ AVOID IN resume
+ - Specific address
+ - Date of Birth
+ - Rating the skills
+ 
+ Be Mindful
+ - Typos
+ - Format it started
+ - As much as we you but not be lost
+ - 1-2 pages is FINE
+ - Maintian a technical tone >> Pizza Cashier
+ - List licenses and certifications
+ - List work experience
+ - TELL YOUR PROFESSIONAL EXPERIENCE
+ - Don't use pronouns "I", "We"
+ - Using list and bullet
+ 
+ ______________________________________________________________________________
+ 
+ Types of Resume in the Hiring Process
+ - Application Resume
+ - Aesthetic Resume
+ - Shared/Hosted Resume
+ - Customizable Resume //Should customize for every application you apply.
+ ______________________________________________________________________________
+
+ Slightly Align to the Employer Brand
+ - Don't borrow the logo, image, do colorful resume.
+ 
+ */
