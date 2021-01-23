18c288778361220727b368adf216a1babb5831b0
Lee McCormick <57606580+Tittaporn@users.noreply.github.com>
2021-01-22 23:59:55 -0600

    Lecture Week3

---
+++ JobHunt_LectuceWeek3Friday.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+var str = "Hello, playground"
+
+/* LECTURE JOB HUNT ON Week 3 Friday at DevMountain
+ 
+ - Active job hunting
+    - 26 application to get called back
+ 
+ Active Job Hunting
+ - More effection, resume visible and applications submitted, and proactive networking
+ Passive Job Hunting
+ - Less immediately effective, Resume simply visible
+_____________________________________________________________
+ - How to identify career boards
+    Type of Career Boards
+    - General
+    - Niche
+    - Job search engine
+    - Resources job board
+    - Company career site
+ _____________________________________________________________
+ 
+ - Challenges to Job Boards
+ - Recruiter Error
+ - Oraganize the Job hunt. >> Applications, Questions, Benefits & Compensation
+ - Benefits of Organizing the Job Hunt. >> Prevent awkward, Show that you ar knowledgeable....
+ - NEVER "Just apply"
+ - Make it More Effective.
+ - Actively seek that "fit" for you
+ 
+ */
